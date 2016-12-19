b3e543220ed28f072338c3629d5db67c2218ca5c
fatma yousef <famyaltimimi@yahoo.com>
2016-12-19 12:56:55 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,12 @@ def   line(katz_deli)
   if katz_deli==[]
     puts "The line is currently empty."
   else
-    puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
-
-end
+    current_line = "The line is currently:"
+        katz_deli.each.with_index(1) do |person, i|
+          current_line << " #{i}. #{person}"
+        end
+        puts current_line
+      end
 end
 def take_a_number(katz_deli, person)
   if katz_deli<<(person)
